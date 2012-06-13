5df800633ddcc9f1251714f3a1ae2e9755607867
Margori Briones <contacto@margori.heliohost.org>
2012-06-12 21:41:19 -0300

    - Cleaning previous architecture

--- config.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-$Configuration['DBServer'] = 'localhost';
-$Configuration['DBPort'] = 3306;
-$Configuration['DBName'] = 'gas';
-$Configuration['DBUsername'] = 'gas';
-$Configuration['DBPassword'] = 'xxxxx';
-?>
