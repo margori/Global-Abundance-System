4a40efc7ab4ce066c1ed2acc9c803cd35aa42045
Margori Briones <contacto@margori.heliohost.org>
2012-06-17 23:17:13 -0300

    - Mispells.

--- protected/messages/en/home.php
+++ protected/messages/en/home.php
@@ -3,7 +3,7 @@ return array (
 		'welcome' => 'Welcome to our Global Abundance System.',
     'why?' => 'A lot of people round the world are willing to help and be helped, to share and receive shares, to serve and be served.',
     'how?' => 'This is a good aproximation on how we could do it:',
-    'what?' => 'Global Abundance System is the place where <b>problem are solved, requirement are fulfilled, needs are sutisfied, missions are acomplished and dreams come true.</b><br /><br/>It\'s up to you...',
+    'what?' => 'Global Abundance System is the place where <b>problem are solved, requirement are fulfilled, needs are satisfied, missions are acomplished and dreams come true.</b><br /><br/>It\'s up to you...',
 		'register' => 'I wanna help too!',
 );
 ?>
--- protected/messages/es/home.php
+++ protected/messages/es/home.php
@@ -3,7 +3,7 @@ return array (
 		'welcome' => 'Bienvenido a nuestro Sistema de Abundancia Global.',
     'why?' => 'Mucha gente alrededor del mundo tienen voluntad de ayudar y ser ayudadas, compartir, servir y ser servidas.',
     'how?' => 'Esta es una explicación de como podriamos lograrlo:',
-    'what?' => 'El sistema de abundancia global es el lugar donde <b>los problemas con resueltos, los requerimientos son completados, las necesidades son satisfechas, las misiones son complatadas y los sueños se hacen realidad.</b><br /><br />Depende de ti...',
+    'what?' => 'El sistema de abundancia global es el lugar donde <b>los problemas con resueltos, los requerimientos son completados, las necesidades son satisfechas, las misiones son completadas y los sueños se hacen realidad.</b><br /><br />Depende de ti...',
 		'register' => 'Yo también quiero ayudar!',
 );
 ?>
