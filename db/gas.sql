b6305c5b954f71888da2265f7e9aaff38d9e4b34
Margori Briones <contacto@margori.heliohost.org>
2012-07-07 21:18:35 -0300

    - DB Changes

--- db/gas.sql
+++ db/gas.sql
@@ -1,16 +1,3 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Base de datos: `gas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `item`
---
-
 CREATE TABLE IF NOT EXISTS `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` text NOT NULL COMMENT 'includes tags prefixed with #',
@@ -22,13 +9,9 @@ CREATE TABLE IF NOT EXISTS `item` (
   `notified` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `item_comment`
---
 
 CREATE TABLE IF NOT EXISTS `item_comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -38,41 +21,30 @@ CREATE TABLE IF NOT EXISTS `item_comment` (
   PRIMARY KEY (`id`),
   KEY `item_id` (`item_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `solution`
---
 
 CREATE TABLE IF NOT EXISTS `solution` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '1' COMMENT '1-new,2-draft,3-completed,4-delivered',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '1-draft,2-completed',
   `item_id` int(11) NOT NULL,
+  `read` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `item_id` (`item_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `solution_archive`
---
 
 CREATE TABLE IF NOT EXISTS `solution_archive` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `solution_item`
---
 
 CREATE TABLE IF NOT EXISTS `solution_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -81,13 +53,17 @@ CREATE TABLE IF NOT EXISTS `solution_item` (
   PRIMARY KEY (`id`),
   KEY `solution_id` (`solution_id`),
   KEY `item_id` (`item_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `user`
---
+CREATE TABLE IF NOT EXISTS `unread_comment` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`comment_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -96,37 +72,24 @@ CREATE TABLE IF NOT EXISTS `user` (
   `real_name` varchar(200) DEFAULT NULL,
   `email` varchar(200) DEFAULT NULL,
   `default_tags` varchar(1000) DEFAULT NULL,
+  `language` varchar(2) NOT NULL DEFAULT 'en',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UQ_username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Restricciones para tablas volcadas
---
+-- --------------------------------------------------------
 
---
--- Filtros para la tabla `item`
---
 ALTER TABLE `item`
   ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
---
--- Filtros para la tabla `item_comment`
---
 ALTER TABLE `item_comment`
-  ADD CONSTRAINT `item_comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `item_comment_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `item_comment_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `item_comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
---
--- Filtros para la tabla `solution`
---
 ALTER TABLE `solution`
-  ADD CONSTRAINT `solution_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `solution_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `solution_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solution_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE;
 
---
--- Filtros para la tabla `solution_item`
---
 ALTER TABLE `solution_item`
-  ADD CONSTRAINT `solution_item_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `solution_item_ibfk_1` FOREIGN KEY (`solution_id`) REFERENCES `solution` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `solution_item_ibfk_1` FOREIGN KEY (`solution_id`) REFERENCES `solution` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solution_item_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE;
